c9af3b77c5fbc34e1ac0651551070920de3905ac
Tinesh <tineshbabukatta@gmail.com>
2021-08-04 21:56:22 +0530

    modify arthimetics shell script

--- arthimetics.sh
+++ arthimetics.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#Shell Script
+#Begin  Shell Script
 
 #read -p "Enter Value of A:" A
 #read -p "Enter Value of B:" B
 
 echo "Sum of Two numbers : $(($1+$2))"
+
+echo "Product of two numbers: $(($1*$2))"
+
+echo "Difference of two numbers: $(($1-$2))"
+
+#End of Shell Script
 
